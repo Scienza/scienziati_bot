41f73178981a77ed89dbf664e25b9084709645cf
AndreaIdini <idibrothers@gmail.com>
2018-10-25 18:07:11 +0200

    implemented database as python dictionary

--- telebot/telebot_test.py
+++ telebot/telebot_test.py
@@ -1,8 +1,20 @@
 import telebot
+import sqlite3
 
 bot = telebot.TeleBot("676490981:AAELlmTlQLD4_1HojhzWIX4yISDrVU5qDmA")
 
 handler = 0
+# database = sqlite3.connect("users.db", check_same_thread=False)
+# sql_command = """
+# CREATE TABLE user ( 
+# id_number INTEGER PRIMARY KEY, 
+# username VARCHAR(30), 
+# first_name VARCHAR(20), 
+# last_name VARCHAR(20),
+# description VARCHAR(300));"""
+# database.execute(sql_command)
+database = {}
+
 intro_mex = "Questo e' il bot del gruppo @scienza, \n\
 /iscrivi iscriviti al database di utenti e a liste di interessi \n\
 /modifica visiona e modifica la propria descrizione \n\
@@ -16,14 +28,27 @@ def send_welcome(message):
 @bot.message_handler(commands=['iscrivi'])
 def start_user_registration(message):
 	global handler
+	global database
 	if not message.from_user.is_bot:
 		bot.reply_to(message, "creazione nuovo record utente...")
 
 		# Save this in database
-		message.from_user.id
-		message.from_user.username
-		message.from_user.first_name
-		message.from_user.last_name
+		database = {str(message.from_user.id) : 
+			{'username' : str(message.from_user.username),
+			'first_name': str(message.from_user.first_name),
+			'last_name' : str(message.from_user.last_name),
+			'description': '',
+			'liste': ''
+			} 
+		}
+
+		# sql_command = ("INSERT INTO user (id_number, username, first_name, last_name)\
+    	# VALUES ("
+		# + str(message.from_user.id) + ", "
+		# + str(message.from_user.username) + ", " 
+		# + str(message.from_user.first_name) + ", "
+		# + str(message.from_user.last_name) + ");")
+		# database.execute(sql_command)
 
 		# this is to define step-by-step subscription
 		handler = 1
@@ -33,10 +58,16 @@ def start_user_registration(message):
 @bot.message_handler(func=lambda m: True if handler == 1 else False)
 def first_registration(message):
 	global handler
+	global database
 	if not message.from_user.is_bot:
-		# if message.from_user.id is the same as before:
-		# Save this in database as description
-		message.text
+		# if message.from_user.id is the same as before save this in database as description
+		database[str(message.from_user.id)]['description'] = message.text
+
+		# sql_command = ("UPDATE users SET description =  "
+		# + message.text 
+		# + "WHEN id_number =" + str(message.from_user.id))
+		# database.execute(sql_command)
+
 		bot.reply_to(message,"A quali liste vuoi iscriverti?")
 		# scrivere lista di liste
 		handler = 2
@@ -44,10 +75,12 @@ def first_registration(message):
 @bot.message_handler(func=lambda m: True if handler == 2 else False)
 def second_registration(message):
 	global handler
+	global database
 	if not message.from_user.is_bot:
 		# if message.from_user.id is the same as before:
 		# Save this in database as subscriptions
-		message.text
+		database[str(message.from_user.id)]['liste'] = message.text
+		print(database)
 		bot.reply_to(message, "Grazie " + message.from_user.first_name)
 		handler = 0
 
